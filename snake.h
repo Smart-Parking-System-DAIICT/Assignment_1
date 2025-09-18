5991df18a7068c66acb8d2aa999246c4f9cb67b2
Aangi-Shah <shahaangi26@gmail.com>
2025-09-18 08:47:27 +0000

    Food should not spawn inside snakes body

--- snake.h
+++ snake.h
@@ -9,12 +9,13 @@
 #include <deque>
 #include <algorithm>
 using namespace std;
+using std::chrono::system_clock;
 using namespace std::this_thread;
-using namespace std::chrono;
 
-char direction='r';
+char direction = 'r';
 
-void input_handler(){
+// input handler
+void input_handler() {
     // change terminal settings
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
@@ -22,26 +23,27 @@ void input_handler(){
     // turn off canonical mode and echo
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
     map<char, char> keymap = {{'d', 'r'}, {'a', 'l'}, {'w', 'u'}, {'s', 'd'}, {'q', 'q'}};
     while (true) {
         char input = getchar();
         if (keymap.find(input) != keymap.end()) {
             direction = keymap[input];
-        } else if (input == 'q'){
+        } else if (input == 'q') {
             exit(0);
         }
     }
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
-void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food){
-    for(size_t i=0;i<size;i++){
-        for(size_t j=0;j<size;j++){
-            if (i == food.first && j == food.second){
+void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food) {
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size; j++) {
+            if (i == food.first && j == food.second) {
                 cout << "🍎";
-            }else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
+            } else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
                 cout << "🐍";
-            }else{
+            } else {
                 cout << "⬜";
             }
         }
@@ -49,54 +51,64 @@ void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food){
     }
 }
 
-pair<int,int> get_next_head(pair<int,int> current, char direction){
-    pair<int, int> next; 
-    if(direction =='r'){
-        next = make_pair(current.first,(current.second+1) % 10);
-    }else if (direction=='l'){
-        next = make_pair(current.first, current.second==0?9:current.second-1);
-    }else if(direction =='d'){
-        next = make_pair((current.first+1)%10,current.second);
-    }else if (direction=='u'){
-        next = make_pair(current.first==0?9:current.first-1, current.second);
+pair<int, int> get_next_head(pair<int, int> current, char direction) {
+    pair<int, int> next;
+    if (direction == 'r') {
+        next = make_pair(current.first, (current.second + 1) % 10);
+    } else if (direction == 'l') {
+        next = make_pair(current.first, current.second == 0 ? 9 : current.second - 1);
+    } else if (direction == 'd') {
+        next = make_pair((current.first + 1) % 10, current.second);
+    } else if (direction == 'u') {
+        next = make_pair(current.first == 0 ? 9 : current.first - 1, current.second);
     }
     return next;
 }
 
-void game_play(){
+pair<int,int> generate_food(int size, deque<pair<int,int>> &snake) {
+    pair<int,int> pos;
+    do {
+        pos = make_pair(rand() % size, rand() % size);
+    } while (find(snake.begin(), snake.end(), pos) != snake.end());
+    return pos;
+}
+
+void game_play() {
     system("clear");
     deque<pair<int, int>> snake;
-    snake.push_back(make_pair(0,0));
+    snake.push_back(make_pair(0, 0));
 
-    pair<int, int> food = make_pair(rand() % 10, rand() % 10);
+    pair<int, int> food = generate_food(10, snake);
 
-    int foods_eaten = 0;               
-    int delay_ms = 500;                
-    const int min_delay = 100;     
+    int foods_eaten = 0;
+    int delay_ms = 500;
+    const int min_delay = 100;
 
-    for(pair<int, int> head=make_pair(0,1);; head = get_next_head(head, direction)){
+    for (pair<int, int> head = make_pair(0, 1);; head = get_next_head(head, direction)) {
         cout << "\033[H";
+
         if (find(snake.begin(), snake.end(), head) != snake.end()) {
             system("clear");
             cout << "Game Over" << endl;
             exit(0);
-        }else if (head.first == food.first && head.second == food.second) {
-            food = make_pair(rand() % 10, rand() % 10);
-            snake.push_back(head);  
-            
+        }
+        else if (head.first == food.first && head.second == food.second) {
+            snake.push_back(head);
+            food = generate_food(10, snake);
             foods_eaten++;
             if (foods_eaten % 10 == 0 && delay_ms > min_delay) {
-                delay_ms -= 50; 
+                delay_ms -= 50;
             }
 
-        }else{
+        } else {
             snake.push_back(head);
             snake.pop_front();
         }
+
         render_game(10, snake, food);
-        cout << "Length of snake: " << snake.size() 
+        cout << "Length of snake: " << snake.size()
              << "    Speed: " << delay_ms << "ms" << endl;
-    
+
         sleep_for(milliseconds(delay_ms));
     }
 }
