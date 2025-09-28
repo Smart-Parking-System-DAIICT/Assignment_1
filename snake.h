798f4bdd0778b715b11fcb783612169d56304e13
Aangi-Shah <shahaangi26@gmail.com>
2025-09-28 14:41:55 +0000

    Refactored code and added test cases

--- snake.h
+++ snake.h
@@ -1,22 +1,83 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 #include <thread>
-#include <stdlib.h>
+#include <cstdlib>
 #include <termios.h>
-#include <unistd.h> // for system clear
+#include <unistd.h>
 #include <map>
 #include <deque>
 #include <algorithm>
 #include <atomic>
+
 using namespace std;
 using namespace std::this_thread;
 
-char direction = 'r';
-atomic<bool> paused(false);
-vector<int> high_scores;
+inline char direction = 'r';
+inline atomic<bool> paused(false);
+inline vector<int> high_scores;
 
-void input_handler() {
+//Pure functions (Testable)
+inline pair<int, int> get_next_head(pair<int, int> current, char dir, int gridSize = 10) {
+    pair<int, int> next = current;
+
+    if (dir == 'r') {
+        next.second = (current.second + 1) % gridSize;
+    } else if (dir == 'l') {
+        next.second = (current.second == 0 ? gridSize - 1 : current.second - 1);
+    } else if (dir == 'd') {
+        next.first = (current.first + 1) % gridSize;
+    } else if (dir == 'u') {
+        next.first = (current.first == 0 ? gridSize - 1 : current.first - 1);
+    }
+    return next;
+}
+
+inline pair<int,int> generate_item(int size, deque<pair<int,int>> &snake, pair<int,int> other) {
+    pair<int,int> pos;
+    do {
+        pos = make_pair(rand() % size, rand() % size);
+    } while (find(snake.begin(), snake.end(), pos) != snake.end() || pos == other);
+    return pos;
+}
+
+inline void update_high_scores(vector<int>& scores, int score) {
+    scores.push_back(score);
+    sort(scores.rbegin(), scores.rend());
+    if (scores.size() > 10) {
+        scores.resize(10);
+    }
+}
+
+//Impure functions (I/O, rendering)
+inline void show_high_scores(const vector<int>& scores) {
+    cout << "\n Top 10 High Scores" << endl;
+    for (size_t i = 0; i < scores.size(); i++) {
+        cout << i + 1 << ". " << scores[i] << endl;
+    }
+}
+
+inline void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food, pair<int, int> poison) {
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size; j++) {
+            if (i == food.first && j == food.second) {
+                cout << "🍎";
+            } else if (i == poison.first && j == poison.second) {
+                cout << "☠️";
+            } else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
+                cout << "🐍";
+            } else {
+                cout << "⬜";
+            }
+        }
+        cout << endl;
+    }
+}
+
+inline void input_handler() {
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
@@ -42,72 +103,18 @@ void input_handler() {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
-void update_high_scores(int score) {
-    high_scores.push_back(score);
-    sort(high_scores.rbegin(), high_scores.rend());
-    if (high_scores.size() > 10) {
-        high_scores.resize(10);
-    }
-}
-
-void show_high_scores() {
-    cout << "\n Top 10 High Scores" << endl;
-    for (size_t i = 0; i < high_scores.size(); i++) {
-        cout << i + 1 << ". " << high_scores[i] << endl;
-    }
-}
-
-void render_game(int size, deque<pair<int, int>> &snake, pair<int, int> food, pair<int, int> poison) {
-    for (size_t i = 0; i < size; i++) {
-        for (size_t j = 0; j < size; j++) {
-            if (i == food.first && j == food.second) {
-                cout << "🍎";
-            } else if (i == poison.first && j == poison.second) {
-                cout << "☠️";
-            } else if (find(snake.begin(), snake.end(), make_pair(int(i), int(j))) != snake.end()) {
-                cout << "🐍";
-            } else {
-                cout << "⬜";
-            }
-        }
-        cout << endl;
-    }
-}
-
-pair<int, int> get_next_head(pair<int, int> current, char direction) {
-    pair<int, int> next;
-    if (direction == 'r') {
-        next = make_pair(current.first, (current.second + 1) % 10);
-    } else if (direction == 'l') {
-        next = make_pair(current.first, current.second == 0 ? 9 : current.second - 1);
-    } else if (direction == 'd') {
-        next = make_pair((current.first + 1) % 10, current.second);
-    } else if (direction == 'u') {
-        next = make_pair(current.first == 0 ? 9 : current.first - 1, current.second);
-    }
-    return next;
-}
-
-pair<int,int> generate_item(int size, deque<pair<int,int>> &snake, pair<int,int> other) {
-    pair<int,int> pos;
-    do {
-        pos = make_pair(rand() % size, rand() % size);
-    } while (find(snake.begin(), snake.end(), pos) != snake.end() || pos == other);
-    return pos;
-}
-
-void game_over(string reason, int score) {
+inline void game_over(string reason, int score) {
     system("clear");
     cout << "Game Over: " << reason << endl;
     cout << "Final Score: " << score << endl;
 
-    update_high_scores(score);
-    show_high_scores();
+    update_high_scores(high_scores, score);
+    show_high_scores(high_scores);
 
     exit(0);
 }
 
-void game_play() {
+inline void game_play() {
     system("clear");
     deque<pair<int, int>> snake;
     snake.push_back(make_pair(0, 0));
@@ -121,7 +128,7 @@ void game_play() {
     const int min_delay = 100;
     int score = 0;
 
-    for (pair<int, int> head = make_pair(0, 1);; head = get_next_head(head, direction)) {
+    for (pair<int, int> head = make_pair(0, 1);; head = get_next_head(head, direction, gridSize)) {
         cout << "\033[H";
 
         if (paused) {
@@ -131,11 +138,11 @@ void game_play() {
         }
 
         if (find(snake.begin(), snake.end(), head) != snake.end()) {
-            game_over("Game Over (You hit yourself)", score);
+            game_over("You hit yourself", score);
         }
 
         if (head == poison) {
-            game_over("Game Over (Ate poison)", score);
+            game_over("Ate poison", score);
         }
 
         if (head == food) {
@@ -160,6 +167,8 @@ void game_play() {
              << "    Speed: " << delay_ms << "ms"
              << "    [Space: Pause/Resume]" << endl;
 
-        sleep_for(milliseconds(delay_ms));
+        sleep_for(std::chrono::milliseconds(delay_ms));
     }
 }
+
+#endif
--- snake_test.cpp
+++ snake_test.cpp
@@ -1,31 +1,72 @@
 #include <gtest/gtest.h>
 #include "snake.h"
+#include <deque>
+#include <vector>
 
+using namespace std;
 
 TEST(SnakeBehaviour, NextHeadRight) {
     pair<int, int> current = make_pair(rand() % 10, rand() % 10);
-    EXPECT_EQ(get_next_head(current, 'r'),make_pair(current.first,current.second+1));
-    
+    EXPECT_EQ(get_next_head(current, 'r'), make_pair(current.first, (current.second + 1) % 10));
 }
 
-
 TEST(SnakeBehaviour, NextHeadLeft) {
-  pair<int, int> current = make_pair(rand() % 10, rand() % 10);
-  EXPECT_EQ(get_next_head(current, 'l'),make_pair(current.first,current.second-1));
-  
+    pair<int, int> current = make_pair(rand() % 10, rand() % 10);
+    EXPECT_EQ(get_next_head(current, 'l'), make_pair(current.first, current.second == 0 ? 9 : current.second - 1));
 }
 
 TEST(SnakeBehaviour, NextHeadUp) {
-  pair<int, int> current = make_pair(rand() % 10, rand() % 10);
-  EXPECT_EQ(get_next_head(current, 'u'),make_pair(current.first-1,current.second));
+    pair<int, int> current = make_pair(rand() % 10, rand() % 10);
+    EXPECT_EQ(get_next_head(current, 'u'), make_pair(current.first == 0 ? 9 : current.first - 1, current.second));
 }
 
 TEST(SnakeBehaviour, NextHeadDown) {
-  pair<int, int> current = make_pair(rand() % 10, rand() % 10);
-  EXPECT_EQ(get_next_head(current, 'd'),make_pair(current.first+1,current.second));
-  
+    pair<int, int> current = make_pair(rand() % 10, rand() % 10);
+    EXPECT_EQ(get_next_head(current, 'd'), make_pair((current.first + 1) % 10, current.second));
 }
 
+TEST(SnakeBehaviour, WrapAroundRightEdge) {
+    pair<int, int> current = {4, 9};
+    EXPECT_EQ(get_next_head(current, 'r', 10), make_pair(4, 0));
+}
+
+TEST(SnakeBehaviour, WrapAroundLeftEdge) {
+    pair<int, int> current = {4, 0};
+    EXPECT_EQ(get_next_head(current, 'l', 10), make_pair(4, 9));
+}
+
+TEST(SnakeBehaviour, WrapAroundTopEdge) {
+    pair<int, int> current = {0, 7};
+    EXPECT_EQ(get_next_head(current, 'u', 10), make_pair(9, 7));
+}
+
+TEST(SnakeBehaviour, WrapAroundBottomEdge) {
+    pair<int, int> current = {9, 7};
+    EXPECT_EQ(get_next_head(current, 'd', 10), make_pair(0, 7));
+}
+
+TEST(SnakeUtils, UpdateHighScoresSorted) {
+    vector<int> scores = {50, 40, 30};
+    update_high_scores(scores, 60);
+    EXPECT_EQ(scores[0], 60);
+    EXPECT_EQ(scores.size(), 4);
+}
+
+TEST(SnakeUtils, HighScoresLimitedTo10) {
+    vector<int> scores = {100,90,80,70,60,50,40,30,20,10};
+    update_high_scores(scores, 95);
+    EXPECT_EQ(scores.size(), 10);
+    EXPECT_EQ(scores[1], 95);
+}
+
+TEST(SnakeUtils, GenerateItemNotOnSnakeOrOther) {
+    deque<pair<int,int>> snake = {{0,0}, {0,1}, {0,2}};
+    pair<int,int> other = {1,1};
+    pair<int,int> item = generate_item(10, snake, other);
+
+    EXPECT_TRUE(find(snake.begin(), snake.end(), item) == snake.end());
+    EXPECT_NE(item, other);
+}
 
 /** 
  * g++ -o my_tests snake_test.cpp -lgtest -lgtest_main -pthread;
